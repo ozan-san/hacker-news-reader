ff6784808662f5236b81f0abdd8f492c8234f2cb
ozan-san <ozan.san@metu.edu.tr>
2021-09-14 23:27:02 +0300

    Fix Main screen's loading state

--- app/src/main/java/com/ozansan/sanews/MainActivity.kt
+++ app/src/main/java/com/ozansan/sanews/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -35,21 +42,48 @@ class MainActivity : ComponentActivity() {
             val isRefreshing by viewModel.isRefreshing.collectAsState()
             SanewsTheme {
                 // A surface container using the 'background' color from the theme
-                SwipeRefresh(
-                    state = rememberSwipeRefreshState(isRefreshing),
-                    onRefresh = { viewModel.getAllStories() }
-                ) {
-                when (topStoriesState.value) {
-                    MainViewModel.NewsEvent.Empty ->
-                        Text("Hello!")
-                    is MainViewModel.NewsEvent.Failure -> {
-                        Text((topStoriesState.value as MainViewModel.NewsEvent.Failure).errorText)
-                    }
-                    MainViewModel.NewsEvent.Loading -> CircularProgressIndicator()
-                    is MainViewModel.NewsEvent.Success -> {
-                        TopStoriesList((topStoriesState.value as MainViewModel.NewsEvent.Success).results)
-                    }
+                Scaffold() {
+                    
                 }
+                when (topStoriesState.value) {
+                    MainViewModel.NewsEvent.Empty -> {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.White),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(32.dp),
+                                color = Color.Gray
+                            )
+                        }
+                    }
+                    is MainViewModel.NewsEvent.Failure -> {
+                        Text(
+                            (topStoriesState.value as MainViewModel.NewsEvent.Failure).errorText,
+                            color = Color.Red
+                        )
+                    }
+                    MainViewModel.NewsEvent.Loading -> {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.size(32.dp),
+                                color = Color.Gray
+                            )
+                        }
+                    }
+                    is MainViewModel.NewsEvent.Success -> {
+                        SwipeRefresh(
+                            state = rememberSwipeRefreshState(isRefreshing),
+                            onRefresh = { viewModel.getAllStories() }
+                        ) {
+                            TopStoriesList((topStoriesState.value as MainViewModel.NewsEvent.Success).results)
+                        }
+                    }
                 }
 
 
